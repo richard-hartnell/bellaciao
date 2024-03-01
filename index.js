1ab12d5366c6a9d2a9b416e0101d844288975ac2
richard-hartnell <richard@richardhartnell.com>
2024-02-29 21:49:27 -0800

    Formatting fixes and menu renaming

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 document.getElementById('shaded-marquis').style.opacity = '1'
 
 const menu = document.getElementById('menu');
-const about = document.getElementById('about');
-const something = document.getElementById('something');
+const pizza = document.getElementById('about');
+const drinks = document.getElementById('something');
 const inside = document.getElementById('inside');
 
 const menu1 = document.getElementById('menu-menu');
-const menu2 = document.getElementById('menu-about');
-const menu3 = document.getElementById('menu-something');
+const menu2 = document.getElementById('menu-food');
+const menu3 = document.getElementById('menu-drinks');
 const menu4 = document.getElementById('menu-inside');
 
 function showMenu(menuId) {
 
     if (menuId === 'menu') {
       menu.style.display = 'block';
-      about.style.display = 'none';
-      something.style.display = 'none';
+      pizza.style.display = 'none';
+      drinks.style.display = 'none';
       inside.style.display = 'none';
       menu1.style.fontWeight = 'bold';
       menu2.style.fontWeight = 'normal';
@@ -23,8 +23,8 @@ function showMenu(menuId) {
       menu4.style.fontWeight = 'normal';
     } else if (menuId === 'about') {
       menu.style.display = 'none';
-      about.style.display = 'block';
-      something.style.display = 'none';
+      pizza.style.display = 'block';
+      drinks.style.display = 'none';
       inside.style.display = 'none';
       menu1.style.fontWeight = 'normal';
       menu2.style.fontWeight = 'bold';
@@ -32,8 +32,8 @@ function showMenu(menuId) {
       menu4.style.fontWeight = 'normal';
     } else if (menuId === 'something') {
       menu.style.display = 'none';
-      about.style.display = 'none';
-      something.style.display = 'block';
+      pizza.style.display = 'none';
+      drinks.style.display = 'block';
       inside.style.display = 'none';
       menu1.style.fontWeight = 'normal';
       menu2.style.fontWeight = 'normal';
@@ -42,8 +42,8 @@ function showMenu(menuId) {
       menu5.style.fontWeight = 'normal';
     } else if (menuId === 'inside') {
       menu.style.display = 'none';
-      about.style.display = 'none';
-      something.style.display = 'none';
+      pizza.style.display = 'none';
+      drinks.style.display = 'none';
       inside.style.display = 'block';
       menu1.style.fontWeight = 'normal';
       menu2.style.fontWeight = 'normal';
